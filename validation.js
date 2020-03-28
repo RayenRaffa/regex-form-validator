f76a747302652eb877a5853c8cd6e11d013f48f3
raffa <raffa.rayen@gmail.com>
2020-03-28 17:06:43 +0100

    add form input's regex

--- validation.js
+++ validation.js
@@ -1,1 +1,7 @@
 // validation script here
+// \w == {a-z A-Z 0-9 _ } . -
+var usernameRegEx   = /[[:alnum:]]{5,12}/;
+var emailRegEx      = /(\w|-|\.)+@[a-z0-9]+\.[a-z]+/;
+var passwordRegEx   = /^[[:alnum:]]*(?=[A-Z])[[:alnum:]]*$/; // still need to check the length
+var phoneRegEx      = /^00216[0-9]{8}$/;
+var cardNumberRegEx = /^[0-9]{2}-[0-9]{2}-[0-9]{2}$/;
