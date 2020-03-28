b0d75aed7707cd383fdb58f1ec304ef9d38d81cf
raffa <raffa.rayen@gmail.com>
2020-03-28 17:46:58 +0100

    improve regex accuracy

--- validation.js
+++ validation.js
@@ -1,7 +1,7 @@
 // validation script here
 // \w == {a-z A-Z 0-9 _ } . -
-var usernameRegEx   = /[[:alnum:]]{5,12}/;
-var emailRegEx      = /(\w|-|\.)+@[a-z0-9]+\.[a-z]+/;
-var passwordRegEx   = /^[[:alnum:]]*(?=[A-Z])[[:alnum:]]*$/; // still need to check the length
-var phoneRegEx      = /^00216[0-9]{8}$/;
+var usernameRegEx   = /^[a-zA-Z0-9]{5,12}$/;
+var emailRegEx      = /^(\w|-|\.)+@[a-z0-9]+(\.[a-z]+)+$/;
+var passwordRegEx   = /^[a-zA-Z0-9]*(?=[A-Z])[a-zA-Z0-9]*$/; // still need to check the length
+var phoneRegEx      = /^(00|\+)216[0-9]{8}$/;
 var cardNumberRegEx = /^[0-9]{2}-[0-9]{2}-[0-9]{2}$/;
