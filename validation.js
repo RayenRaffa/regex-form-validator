2350fa8121b2b40a756ca01fc0b23286cd314b3d
raffa <raffa.rayen@gmail.com>
2020-03-28 17:56:02 +0100

    add input checking method

--- validation.js
+++ validation.js
@@ -5,3 +5,27 @@ var emailRegEx      = /^(\w|-|\.)+@[a-z0-9]+(\.[a-z]+)+$/;
 var passwordRegEx   = /^[a-zA-Z0-9]*(?=[A-Z])[a-zA-Z0-9]*$/; // still need to check the length
 var phoneRegEx      = /^(00|\+)216[0-9]{8}$/;
 var cardNumberRegEx = /^[0-9]{2}-[0-9]{2}-[0-9]{2}$/;
+
+
+var checkInput = function (input, type) {
+    switch (type) {
+        case 'username' :
+            if (usernameRegEx.test(input)) { return true; }
+            break;
+        case 'email' :
+            if (emailRegEx.test(input)) { return true; }
+            break;
+        case 'password' :
+            if (passwordRegEx.test(input)) { return true; }
+            break;
+        case 'phoneNumber' :
+            if (phoneRegEx.test(input)) { return true; }
+            break;
+        case 'cardNumber' :
+            if (cardNumberRegEx.test(input)) { return true; }
+            break;
+        default :
+            alert('Invalid type to check against, please make sure you provided a correct input type');
+    }
+    return false;
+}
