4baee86a7c3d1b873d6cbb9c0ccaa2eb14bd2784
raffa <raffa.rayen@gmail.com>
2020-03-29 21:07:35 +0100

    add input validation method

--- form.js
+++ form.js
@@ -1,16 +1,18 @@
 let form    = document.createElement("form");
-let formDiv = document.createElement('div');
+let formDiv = document.getElementById('formDiv');
 formDiv.appendChild(form);
 
 let userNameField = document.createElement('input');
 userNameField.setAttribute('type','text');
 userNameField.setAttribute('name','username');
-userNameField.setAttribute('value','Username');
-userNameField.setAttribute('onFocus', 'if (this.value === "Username") { this.value = ""; }');
-userNameField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Username"};');
+userNameField.setAttribute('id','username');
+userNameField.setAttribute('placeholder','Username');
+// userNameField.setAttribute('onFocus', 'if (this.value === "Username") { this.value = ""; }');
+// userNameField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Username"};');
 
 let userNameLabel = document.createElement('label');
 userNameLabel.setAttribute('for',userNameField.name);
+userNameLabel.setAttribute('id','usernameLabel');
 userNameLabelText = document.createTextNode('Must be alphanumeric & contain 5-12 characters e.g. walid123');
 userNameLabel.appendChild(userNameLabelText);
 
@@ -19,26 +21,30 @@ userNameLabel.appendChild(userNameLabelText);
 let emailField = document.createElement('input');
 emailField.setAttribute('type','text');
 emailField.setAttribute('name','email');
-emailField.setAttribute('value','Email');
-emailField.setAttribute('onFocus', 'if (this.value === "Email") { this.value = ""; }');
-emailField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Email"};');
+emailField.setAttribute('id','email');
+emailField.setAttribute('placeholder','Email');
+// emailField.setAttribute('onFocus', 'if (this.value === "Email") { this.value = ""; }');
+// emailField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Email"};');
 
 let emailLabel = document.createElement('label');
 emailLabel.setAttribute('for',emailField.name);
+emailLabel.setAttribute('id','emailLabel');
 emailLabelText = document.createTextNode('Must be a valid email address e.g. me@mydomain.com');
 emailLabel.appendChild(emailLabelText);
 
 
 
 let passwordField = document.createElement('input');
-passwordField.setAttribute('type','text');
+passwordField.setAttribute('type','password');
 passwordField.setAttribute('name','password');
-passwordField.setAttribute('value','Password');
-passwordField.setAttribute('onFocus', 'if (this.value === "Password") { this.type = "password"; this.value = ""; }');
-passwordField.setAttribute('onBlur',  'if (this.value === "") {this.type ="text"; this.value = "Password"};');
+passwordField.setAttribute('id','password');
+passwordField.setAttribute('placeholder','Password');
+// passwordField.setAttribute('onFocus', 'if (this.value === "Password") { this.type = "password"; this.value = ""; }');
+// passwordField.setAttribute('onBlur',  'if (this.value === "") {this.type ="text"; this.value = "Password"};');
 
 let passwordLabel = document.createElement('label');
 passwordLabel.setAttribute('for',passwordField.name);
+passwordLabel.setAttribute('id','passwordLabel');
 passwordLabelText = document.createTextNode('Must be alphanumeric & contain 5-12 characters\nMust have upper & lower case e.g. Walid123');
 passwordLabel.appendChild(passwordLabelText);
 
@@ -47,12 +53,14 @@ passwordLabel.appendChild(passwordLabelText);
 let phoneField = document.createElement('input');
 phoneField.setAttribute('type','text');
 phoneField.setAttribute('name','phoneNumber');
-phoneField.setAttribute('value','Phone number');
-phoneField.setAttribute('onFocus', 'if (this.value === "Phone number") { this.value = ""; }');
-phoneField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Phone number"};');
+phoneField.setAttribute('id','phoneNumber');
+phoneField.setAttribute('placeholder','Phone number');
+// phoneField.setAttribute('onFocus', 'if (this.value === "Phone number") { this.value = ""; }');
+// phoneField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Phone number"};');
 
 let phoneLabel = document.createElement('label');
 phoneLabel.setAttribute('for',phoneField.name);
+phoneLabel.setAttribute('id','phoneNumberLabel');
 phoneLabelText = document.createTextNode('Must be a valid Tunisia number e.g. 0021620200200');
 phoneLabel.appendChild(phoneLabelText);
 
@@ -61,12 +69,14 @@ phoneLabel.appendChild(phoneLabelText);
 let cardNumField = document.createElement('input');
 cardNumField.setAttribute('type','text');
 cardNumField.setAttribute('name','cardNumber');
-cardNumField.setAttribute('value','Card number');
-cardNumField.setAttribute('onFocus', 'if (this.value === "Card number") { this.value = ""; }');
-cardNumField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Card Number"};');
+cardNumField.setAttribute('id','cardNumber');
+cardNumField.setAttribute('placeholder','Card number');
+// cardNumField.setAttribute('onFocus', 'if (this.value === "Card number") { this.value = ""; }');
+// cardNumField.setAttribute('onBlur',  'if (this.value === "") {this.value = "Card Number"};');
 
 let cardNumLabel = document.createElement('label');
 cardNumLabel.setAttribute('for',cardNumField.name);
+cardNumLabel.setAttribute('id','cardNumberLabel');
 cardNumLabelText = document.createTextNode('Must be numeric (6 digits) between each two digits - e.g. 12-35-89');
 cardNumLabel.appendChild(cardNumLabelText);
 
@@ -74,13 +84,17 @@ cardNumLabel.appendChild(cardNumLabelText);
 
 form.appendChild(userNameField);
 form.appendChild(userNameLabel);
+
 form.appendChild(emailField);
 form.appendChild(emailLabel);
+
 form.appendChild(passwordField);
 form.appendChild(passwordLabel);
+
 form.appendChild(phoneField);
 form.appendChild(phoneLabel);
+
 form.appendChild(cardNumField);
 form.appendChild(cardNumLabel);
 
-document.body.appendChild(formDiv);
+// document.body.appendChild(formDiv);
--- validation.js
+++ validation.js
@@ -6,26 +6,62 @@ var passwordRegEx   = /^[a-zA-Z0-9]*(?=[A-Z])[a-zA-Z0-9]*$/; // still need to ch
 var phoneRegEx      = /^(00|\+)216[0-9]{8}$/;
 var cardNumberRegEx = /^[0-9]{2}-[0-9]{2}-[0-9]{2}$/;
 
-
 var checkInput = function (input, type) {
     switch (type) {
         case 'username' :
-            if (usernameRegEx.test(input)) { return true; }
-            break;
+        if (usernameRegEx.test(input)) { return true; }
+        break;
         case 'email' :
-            if (emailRegEx.test(input)) { return true; }
-            break;
+        if (emailRegEx.test(input)) { return true; }
+        break;
         case 'password' :
-            if (passwordRegEx.test(input)) { return true; }
-            break;
+        if (passwordRegEx.test(input)) { return true; }
+        break;
         case 'phoneNumber' :
-            if (phoneRegEx.test(input)) { return true; }
-            break;
+        if (phoneRegEx.test(input)) { return true; }
+        break;
         case 'cardNumber' :
-            if (cardNumberRegEx.test(input)) { return true; }
-            break;
+        if (cardNumberRegEx.test(input)) { return true; }
+        break;
         default :
-            alert('Invalid type to check against, please make sure you provided a correct input type');
+        alert('Invalid type to check against, please make sure you provided a correct input type');
     }
     return false;
 }
+
+var updateState = function(tagId, type) {
+    if (checkInput($(tagId).val(), type)) {
+        $(tagId).css('border-color',    '#3cba54');
+        $(tagId + 'Label').css('color', '#3cba54');
+    } else {
+        $(tagId).css('border-color',    '#db3236');
+        $(tagId + 'Label').css('color', '#db3236');
+    }
+    if ($(tagId).val() === '') {
+        $(tagId).css('border-color',    '#455A64');
+        $(tagId + 'Label').css('color', '#455A64');
+    }
+}
+
+$(document).ready(function() {
+    $('#username').keyup(function() {
+        console.log('username');
+        updateState('#username', 'username');
+    });
+
+    $('#email').keyup(function() {
+        updateState('#email', 'email');
+    });
+
+    $('#password').keyup(function() {
+        updateState('#password', 'password');
+    });
+
+    $('#phoneNumber').keyup(function() {
+        updateState('#phoneNumber', 'phoneNumber');
+    });
+
+    $('#cardNumber').keyup(function() {
+        updateState('#cardNumber', 'cardNumber');
+    });
+});
